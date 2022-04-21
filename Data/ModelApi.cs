aa1b04aeded763f8d4e019dc31c9c141085b1ecf
Wiktor Malavasi <malavasiwixus@gmail.com>
2022-04-21 18:51:59 +0200

    Model Api changes

---
+++ Data/ModelApi.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public abstract class DataApi
+    {
+        public static DataApi CreateAPI()
+        {
+            return new DataLayer();
+        }
+
+        public class DataLayer : DataApi
+        {
+            public DataLayer()
+            {
+
+            }
+        }
+    }
+
+
+}
