1823aa80d7582132928ccff00b2624bf21330768
Wiktor Malavasi <malavasiwixus@gmail.com>
2022-05-18 21:05:05 +0200

    Added Logic Ball counterpart

--- Logic/LogicBall.cs
+++ Logic/LogicBall.cs
@@ -1,13 +1,94 @@
 ﻿using System.ComponentModel;
+using Data;
 
 namespace Logic
 {
-    public interface IBall : INotifyPropertyChanged
+    
+    public class LogicBall : INotifyPropertyChanged
     {
-        float X { get; }
-        float Y { get; }
-        int Radius { get; }
+        private readonly Ball ball;
 
-        void RaisePropertyChanged(string propertyName = null);
+        public  LogicBall(Ball ball)
+        {
+            this.ball = ball;
+            ball.PropertyChanged += CordsChange;
+        }
+
+        public void CordsChange(object sender, PropertyChangedEventArgs e)
+        {
+            RaisePropertyChanged("Coordinates");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisePropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public float X
+        {
+            get => ball.X;
+            set
+            {
+                ball.X = value;
+                RaisePropertyChanged(nameof(X));
+            }
+        }
+
+        public float Y
+        {
+            get => ball.Y;
+            set
+            {
+                ball.Y = value;
+                RaisePropertyChanged(nameof(Y));
+            }
+
+        }
+
+        public int Radius
+        {
+            get => ball.Radius;
+            set
+            {
+                if (value > 0)
+                {
+                    ball.Radius = value;
+                }
+
+                else
+                {
+                    throw new System.ArgumentException("Panie, to dodatnie ma byc");
+                }
+
+            }
+        }
+
+        public float XSpeed
+        {
+            get => ball.XSpeed;
+            set
+            {
+                ball.XSpeed = value;
+            }
+        }
+
+        public float YSpeed
+        {
+            get => ball.YSpeed;
+            set
+            {
+                ball.YSpeed = value;
+            }
+        }
+
+        public int Mass
+        {
+            get
+            {
+                return ball.Mass;
+            }
+        }
     }
 }
